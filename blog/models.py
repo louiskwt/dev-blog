bf2748c04c310d5d183284475a95c411c94a7a5f
Louis Tsang <kawingt2@gmail.com>
2022-07-02 23:36:58 +0800

    set up blog detail page

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    slud = models.SlugField()
+    slug = models.SlugField()
     intro = models.TextField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

ae53746e516b4fb0d7b048660496d2fbda026bf1
Louis Tsang <kawingt2@gmail.com>
2022-07-03 23:16:36 +0800

    worked on category

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,21 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -11,6 +25,8 @@ class Post(models.Model):
     class Meta:
         ordering = ('-created_at',)
 
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)

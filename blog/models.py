13f875da0156b0c4ae52e19a6be9da3d5c2e9c26
Louis Tsang <kawingt2@gmail.com>
2022-07-09 22:59:25 +0800

    added sitemap and robots.txt

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return '/%s/' % self.slug
 class Post(models.Model):
     ACTIVE = 'active'
     DRAFT = 'draft'
@@ -37,6 +40,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return '/%s/%s/' % (self.category.slug, self.slug)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
